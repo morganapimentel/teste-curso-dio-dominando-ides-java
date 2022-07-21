e22708ea443ac88ae01dbe43bcb366c998510f19
Morgana <morga.pimentel@gmail.com>
2022-07-21 19:03:44 -0300

    criacao de calculadora

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,40 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a, b);
+		int multiplicacao = multiplicacao(a,b);
+		int divisao = divisao(a, b);
+		
+		System.out.println("O resultado da soma é: " + soma);
+		System.out.println("O resultado da subtração é: " + subtracao);
+		System.out.println("O resultado da multiplicação é: " + multiplicacao);
+		System.out.println("O resultado da divisão é:" + divisao);
+}
+	
+	public static int soma(int a, int b) {
+		return a + b;
+	}
+	public static int subtracao(int a, int b) {
+		return a - b;
+	}
+	public static int multiplicacao(int a, int b) {
+		return a * b;
+	}
+	public static int divisao(int a, int b) {
+		return a / b;
+	}
+}
